0b58836f2ff1daf2a2e4737c306f4910ffbddff4
bluewhale507 <jec9663@gmail.com>
2022-06-25 10:39:16 +0900

    third commit

--- bluewhale507/src/MyComponent.js
+++ bluewhale507/src/MyComponent.js
@@ -1,24 +1,38 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Component } from 'react';
 
-const MyComponent = ({name, number, children}) => {
-    return (
-        <Fragment>        
+
+
+export default class MyComponent extends Component {
+
+    static defalutProps = {
+        name : '기본이름'
+    };
+
+    static propTypes = {
+        name : PropTypes.string,
+        number : PropTypes.number.isRequired
+    }
+
+    render() {
+        const { name, number, children } = this.props;
+        return(
+            <Fragment>        
             <div>안녕하세요 제 이름은 {name} 입니다.</div>
             <div>children 값은 {children} 입니다.</div>
             <br/>
             숫자 : {number}
         </Fragment>
-    );
+        );
+    }
 }
 
-MyComponent.defaultProps = {
-    name : '기본이름'
-}
+// MyComponent.defaultProps = {
+//     name : '기본이름'
+// }
 
-MyComponent.propTypes = { 
-    name : PropTypes.string,
-    number : PropTypes.number.isRequired
-}
-
-export default MyComponent;
+// MyComponent.propTypes = { 
+//     name : PropTypes.string,
+//     number : PropTypes.number.isRequired
+// }
