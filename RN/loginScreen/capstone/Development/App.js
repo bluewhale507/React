9a089352ab37fecb54b1fa7c9d27ab243a7a02d4
bluewhale507 <jec9663@gmail.com>
2022-07-04 14:11:36 +0900

    dir adjustment

---
+++ RN/loginScreen/capstone/Development/App.js
@@ -0,0 +1,12 @@
+import "react-native-gesture-handler";
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import Navigator from "./screen/navigation/navigation.js";
+
+class App extends Component {
+  render() {
+    return <Navigator />;
+  }
+}
+
+export default App;
