433fc84f078cb19af9538deede3d91c1ba7a88bc
bluewhale507 <jec9663@gmail.com>
2022-07-05 15:53:14 +0900

    chap05-ref

--- bluewhale507/src/App.js
+++ bluewhale507/src/App.js
@@ -3,6 +3,8 @@ import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
 import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
@@ -14,6 +16,11 @@ class App extends Component {
         <Counter></Counter>
         <Say></Say>
         <EventPractice />
+        <ValidationSample></ValidationSample>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
       </div>
     );
   }
