d5d5e067c9cc7aaafceb57de2b499827e10c8cb8
bluewhale507 <jec9663@gmail.com>
2022-07-09 12:14:46 +0900

    chap 07

--- bluewhale507/src/App.js
+++ bluewhale507/src/App.js
@@ -6,8 +6,24 @@ import EventPractice from "./EventPractice";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
 import { IterationSample } from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div>
@@ -24,6 +40,10 @@ class App extends Component {
         </button>
         <button onClick={() => this.scrollBox.scrollToTop()}>맨 위로</button>
         <IterationSample />
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+          <button onClick={this.handleClick}>랜덤색상</button>
+        </ErrorBoundary>
       </div>
     );
   }
